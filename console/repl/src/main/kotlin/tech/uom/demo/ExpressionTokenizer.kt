bdd727a1f750f9390216fd40dd06367123ae4437
David Stibbe <david@davidstibbe.nl>
2019-06-12 23:49:36 +0200

    Fized tokenizer

--- console/repl/src/main/kotlin/tech/uom/demo/ExpressionTokenizer.kt
+++ console/repl/src/main/kotlin/tech/uom/demo/ExpressionTokenizer.kt
@@ -3,6 +3,10 @@ package tech.uom.demo
 class ExpressionTokenizer {
 
     fun tokenize(input: String): List<Token> {
-        return input.split(" ").map { Token(it) }
+        return input.trim().split(regex).map { Token(it) }
+    }
+
+    companion object {
+        private val regex = """\s+""".toRegex()
     }
 }
