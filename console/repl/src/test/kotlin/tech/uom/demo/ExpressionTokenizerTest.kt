1d8734470a5fdea84c7147f47bceb70a5d2d1c8a
David Stibbe <david@davidstibbe.nl>
2019-06-12 23:33:01 +0200

    Updated test

--- console/repl/src/test/kotlin/tech/uom/demo/ExpressionTokenizerTest.kt
+++ console/repl/src/test/kotlin/tech/uom/demo/ExpressionTokenizerTest.kt
@@ -1,6 +1,8 @@
 package tech.uom.demo
 
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.core.IsCollectionContaining
+import org.hamcrest.core.IsCollectionContaining.*
 import org.junit.Assert.assertThat
 import org.junit.Test
 
@@ -8,8 +10,9 @@ class ExpressionTokenizerTest {
 
     @Test
     fun `can tokenize the input`() {
-        ExpressionTokenizer().tokenize("2 m + 4 kasd").run {
-            assertThat(this, IsCollectionContaining.hasItems(
+        ExpressionTokenizer().tokenize("            2     m    +     4    kasd             ").run {
+            assertThat(size, `is`(5))
+            assertThat(this, hasItems(
                     Token("2")
                     , Token("m")
                     , Token("+")
