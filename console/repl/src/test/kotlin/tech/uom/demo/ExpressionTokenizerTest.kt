6529ca21630eb01a7ee51413b377372b7e48ac07
David Stibbe <david@davidstibbe.nl>
2019-06-12 23:31:08 +0200

    Cleaned up test

--- console/repl/src/test/kotlin/tech/uom/demo/ExpressionTokenizerTest.kt
+++ console/repl/src/test/kotlin/tech/uom/demo/ExpressionTokenizerTest.kt
@@ -1,16 +1,22 @@
 package tech.uom.demo
 
-import org.junit.Assert.assertEquals
+import org.hamcrest.core.IsCollectionContaining
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class ExpressionTokenizerTest {
 
     @Test
     fun `can tokenize the input`() {
-        ExpressionTokenizer().tokenize("2 m").run {
-            assertEquals("Expected 2 tokens", size, 2)
-            assertEquals("Expected 2 tokens", get(0), Token("2"))
-            assertEquals("Expected 2 tokens", get(1), Token("m"))
+        ExpressionTokenizer().tokenize("2 m + 4 kasd").run {
+            assertThat(this, IsCollectionContaining.hasItems(
+                    Token("2")
+                    , Token("m")
+                    , Token("+")
+                    , Token("4")
+                    , Token("kasd")
+            ))
+
         }
     }
 
